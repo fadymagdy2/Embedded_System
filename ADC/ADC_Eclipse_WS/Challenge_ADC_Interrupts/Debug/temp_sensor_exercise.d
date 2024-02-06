d3cdc60dd8c1c724ff99618942c60ac999903312
fady <fadymagdy430@gmail.com>
2024-02-06 19:48:09 +0200

    ADC

---
+++ ADC/ADC_Eclipse_WS/Challenge_ADC_Interrupts/Debug/temp_sensor_exercise.d
@@ -0,0 +1,10 @@
+temp_sensor_exercise.o temp_sensor_exercise.o: ../temp_sensor_exercise.c \
+  ../adc.h ../std_types.h ../lcd.h ../lm35_sensor.h
+
+../adc.h:
+
+../std_types.h:
+
+../lcd.h:
+
+../lm35_sensor.h:
